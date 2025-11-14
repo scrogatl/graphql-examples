68f22f178bf0894d17d1aabe614096a92c34fb23
scrogatl <scott@scottandrewrogers.com>
2025-11-13 19:36:00 -0500

    retrieve perms and create new role with them

---
+++ create_role.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import sys
+import argparse
+
+
+user_key = sys.argv[1]
+permission_file = sys.argv[2]
+
+parser = argparse.ArgumentParser(description = "Description for my parser")
+parser.add_argument('user_key')
+parser.add_argument('permission_file')
+argument = parser.parse_args()
+
+
+def getOrgId():
+    payload = '''
+    {
+      actor {
+        organization {
+          id
+        }
+      }
+    }
+    '''
+    endpoint = "https://api.newrelic.com/graphql"
+    headers = {'Content-Type': 'application/json', 'API-Key': f'{user_key}'}
+    response = requests.post(endpoint, headers=headers, json={'query': payload})
+    if response.status_code == 200:
+        dict_response = json.loads(response.content)
+        return dict_response['data']['actor']['organization']['id']
+    else:
+        # raise an error with a HTTP response code
+        raise Exception(f'Nerdgraph request failed with a {response.status_code}.')
+    return None
+
+def loadPermissions(filename):
+    try:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+    except json.JSONDecodeError:
+        print("Error: Could not decode JSON from 'data.json'. Check file format.")
+    perm_ids = []
+    for item in data['data']['customerAdministration']['permissions']['items']:
+        perm_ids.append(int(item['id']))
+        print(f"Permission ID: {item['id']}, Feature: {item['feature']}, Category: {item['category']}, Product: {item['product']}")
+    return perm_ids
+
+def createRole(role_name, org_id, permissions):
+    payload = '''
+    mutation {{ 
+      customRoleCreate(
+      container: {{
+        id: "{org_id}"
+        type: "ORANIZATION"
+      }}
+        name: "{role_name}"
+          permissionIds: {permissions}
+          scope: "account"
+        ) {{
+          id
+        }}
+      }}
+      '''.format(org_id=org_id, role_name=role_name, permissions=json.dumps(permissions))
+    print(payload)
+    # exit(1)
+    endpoint = "https://api.newrelic.com/graphql"
+    headers = {'Content-Type': 'application/json', 'API-Key': f'{user_key}'}
+    response = requests.post(endpoint, headers=headers, json={'query': payload})
+    if response.status_code == 200:
+        dict_response = json.loads(response.content)
+        print(f"No Error {dict_response}")
+    else:
+        # raise an error with a HTTP response code
+        raise Exception(f'Nerdgraph request failed with a {response.status_code}.')
+    return None
+    
+if __name__ == '__main__':
+    org_id = getOrgId()
+    permissions = loadPermissions(permission_file)
+    pretty_permissions = json.dumps(permissions, indent=4)
+    print(pretty_permissions)
+    print(f"Organization ID: {org_id}")
+    createRole("test role 4", org_id, permissions)
---
+++ get_permissions.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import sys
+import argparse
+
+
+user_key = sys.argv[1]
+
+parser = argparse.ArgumentParser(description = "Description for my parser")
+parser.add_argument('user_key')
+parser.add_argument("-D", "--delete", required=False, action='store_true')
+argument = parser.parse_args()
+
+def getPermissions():
+    payload = '''
+    {
+      customerAdministration {
+        permissions {
+          items {
+            feature
+            category
+            id
+            product
+          }
+        }
+      }
+    }   
+  '''
+    endpoint = "https://api.newrelic.com/graphql"
+    headers = {'Content-Type': 'application/json', 'API-Key': f'{user_key}'}
+    response = requests.post(endpoint, headers=headers, json={'query': payload})
+    if response.status_code == 200:
+        dict_response = json.loads(response.content)
+        return dict_response
+    else:
+        # raise an error with a HTTP response code
+        raise Exception(f'Nerdgraph request failed with a {response.status_code}.')
+
+
+    
+if __name__ == '__main__':
+    pretty_permissions = json.dumps(getPermissions(), indent=4)
+    print(pretty_permissions)
